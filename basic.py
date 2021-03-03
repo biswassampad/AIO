f3d0c6b195f0f3b7792b62dcb7aadb561c7e4433
Biswas Sampad <biswa.satpathy@iserveu.in>
2021-03-03 13:27:47 +0530

    float recog issue fixed

--- basic.py
+++ basic.py
@@ -13,7 +13,7 @@ class Error:
         result = f'{self.error_name}:{self.details}'
         return result
 
-class IllegalCharError:
+class IllegalCharError(Error):
     def __init__(self,details):
         super().__init__('Illegal Character',details)
 
@@ -79,15 +79,15 @@ class Lexer:
             else:
                 char = self.current_char
                 self.advance()
-                return[], IllegalCharacterError("'" + char + "'")
+                return[], IllegalCharError("'" + char + "'")
 
-        return tokens,None
+        return tokens, None
     
     def make_number(self):
         num_str = ''
         dot_count = 0
 
-        while self.current_char != None and self.current_char in DIGITS:
+        while self.current_char != None and self.current_char in DIGITS + '.':
             if self.current_char == '.':
                 if dot_count == 1 : break
                 dot_count += 1
